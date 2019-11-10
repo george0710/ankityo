409a2b28881d4cb696d4a2ea98ae76cdf6f9c2da
george0710 <geojihdev@gmail.com>
2019-11-10 23:47:51 +0900

    firebaseのapiキーをgitignoreに設定

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ import 'vuetify/dist/vuetify.min.css';
 import '@mdi/font/css/materialdesignicons.css'; // Ensure you are using css-loader
 import 'vuetify/dist/vuetify.min.css';
 import firebase from 'firebase';
-
+import { firebaseApiKey } from '../env.config.js';
 Vue.use(VueRouter);
 
 Vue.config.productionTip = false;
@@ -23,8 +23,9 @@ export default new Vuetify({
   },
 });
 
+
 const firebaseConfig = {
-  apiKey: 'AIzaSyBtH5zMzUp1vKQnpVcb_vR1hKA0djU5jLg',
+  apiKey: firebaseApiKey,
   authDomain: 'ankityo-184c3.firebaseapp.com',
   databaseURL: 'https://ankityo-184c3.firebaseio.com',
   projectId: 'ankityo-184c3',
